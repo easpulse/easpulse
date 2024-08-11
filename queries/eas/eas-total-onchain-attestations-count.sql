9b0e639efb0dc091e01a02448035aa5f9f106813
Ivan Molto <ivanmolto@gmail.com>
2024-08-11 18:01:12 +0200

    chore: Add EAS queries

---
+++ queries/eas/eas-total-onchain-attestations-count.sql
@@ -0,0 +1,44 @@
+SELECT
+  (
+    (
+      SELECT
+        COUNT(*)
+      FROM
+        ethereum_attestation_service_ethereum.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        attestationstation_v1_optimism.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        attestationstation_v1_base.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        arbitrum_eas_arbitrum.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        attestationstation_v1_linea.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        polygon_eas_polygon.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        eas_scroll.EAS_evt_Attested
+    ) + (
+      SELECT
+        COUNT(*)
+      FROM
+        attestationstation_v1_zksync.EAS_evt_Attested
+    )
+  ) AS "Total Attestations";
---
+++ queries/eas/eas-total-onchain-revocations-count.sql
@@ -0,0 +1,42 @@
+SELECT
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      attestationstation_v1_optimism.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      attestationstation_v1_base.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      attestationstation_v1_linea.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      arbitrum_eas_arbitrum.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      polygon_eas_polygon.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      ethereum_attestation_service_ethereum.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      eas_scroll.EAS_evt_Revoked
+  ) + (
+    SELECT
+      COUNT(*)
+    FROM
+      attestationstation_v1_zksync.EAS_evt_Revoked
+  ) AS "Total Revocations"
